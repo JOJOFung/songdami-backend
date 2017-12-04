a479ffb2829317355798def22c08ba66fff0d8c6
冯姣姣 <fengjiaojiao@fengjiaojiaodeMacBook-Pro.local>
2017-12-04 17:14:55 +0800

    Add openid to DB table

--- db/db.js
+++ db/db.js
@@ -1,20 +1,26 @@
+var wx = require('../wx/wx.js');
 var sqlite3 = require('sqlite3').verbose();
 var db = null;
 
 function save2DB(oOrder) {
     _getOrderTable();
+    wx.getOpenId(oOrder.code, _insertOrder, oOrder);
+}
+
+function _insertOrder(sOpenId, oOrder) {
     db.serialize(function () {
-        //TODO get openid according to oOrder.code.
-        db.run("INSERT INTO itemorder VALUES (?, ?, ?, ?, ?, ?)",
+        db.run("INSERT INTO itemorder VALUES (?, ?, ?, ?, ?, ?, ?)",
+            sOpenId,
             oOrder.name + oOrder.sex,
             oOrder.telephone,
             oOrder.itemName,
             oOrder.address,
             oOrder.date,
-            new Date());
+            new Date()
+        );
 
         db.each("SELECT * FROM itemorder", function (err, row) {
-            // console.log(row);
+            console.log(row);
         });
     });
     db.close();
@@ -23,7 +29,7 @@ function save2DB(oOrder) {
 function _getOrderTable() {
     db = new sqlite3.Database(':memory:');
     // db.run("DROP TABLE IF EXISTS itemorder");
-    db.run("CREATE TABLE IF NOT EXISTS itemorder (name TEXT, telephone TEXT, item TEXT, address TEXT, deliverDate date, createTime datetime)");
+    db.run("CREATE TABLE IF NOT EXISTS itemorder (openId TEXT PRIMARY KEY, name TEXT, telephone TEXT, item TEXT, address TEXT, deliverDate date, createTime datetime)");
 }
 
 module.exports = {save2DB: save2DB};
